5bace8d6b6d54d05d7a45b55a1aec29075f23835
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-24 14:43:46 -0500

    Added posts to city page

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { cities } from './cities';
+import { cities } from '../data/cities';
 
 @Component({
   selector: 'app-cities',
