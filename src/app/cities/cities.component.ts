5d1353531463ea8f764d578b9fee959372a6c049
Maksym Zinchenko <maklaut98@gmail.com>
2023-05-24 14:30:33 -0500

    Highlight current city

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { cities} from './cities'
+import { cities } from './cities';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.css']
+  styleUrls: ['./cities.component.css'],
 })
 export class CitiesComponent implements OnInit {
-  cityIndex: string|null = '';
+  citiesList: any = cities;
+  cityIndex: string | null = '';
   city: any;
 
-  constructor(private route: ActivatedRoute) {
-
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.cityIndex = params.get('id');
-      this.city = cities.find(city => {
+      this.city = this.citiesList.find((city: any) => {
         let paramId: string = params.get('id') || '';
-        return city.id === parseInt(paramId)
-    })
-  })
-}
-
-
+        return city.id === parseInt(paramId);
+      });
+    });
+  }
 }
--- src/app/cities/cities.ts
+++ src/app/cities/cities.ts
@@ -1,41 +1,41 @@
 interface Cities {
-    id: number,
-    name: string,
-    photo: string,
-    country: string
-
-    
+  id: number;
+  name: string;
+  photo: string;
+  country: string;
 }
 
-// This the 
+// This the
 export const cities: Cities[] = [
-    
-    {id: 1,
-        name: 'San Francisco',
-        photo: '',
-        country: 'USA'
-        
-        
-    },
+  {
+    id: 1,
+    name: 'San Francisco',
+    photo:
+      'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/san-francisco-golden-gate-bridge-1x1-gregory-ballos.jpg',
+    country: 'USA',
+  },
 
-    {
-        id: 2,
-        name: 'London',
-        photo: '',
-        country: 'UK'
-    },
+  {
+    id: 2,
+    name: 'London',
+    photo:
+      'https://i2-prod.mirror.co.uk/incoming/article28871026.ece/ALTERNATES/s1200c/0_London-at-sunset.jpg',
+    country: 'UK',
+  },
 
-    {
-        id: 3,
-        name: 'Seattle',
-        photo: '',
-        country: 'USA'
-    },
+  {
+    id: 3,
+    name: 'Seattle',
+    photo:
+      'https://imageio.forbes.com/i-forbesimg/media/lists/places/seattle-wa_416x416.jpg?format=jpg&height=416&width=416&fit=bounds',
+    country: 'USA',
+  },
 
-    {
-        id: 4,
-        name: 'Sydney',
-        photo: '',
-        country: 'Australia'
-    }
-]
+  {
+    id: 4,
+    name: 'Sydney',
+    photo:
+      'https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1600/1600/90/media/abd/refresh/asia-pacific/3-night-sydney-vacations/harbour-1x1.jpg?cb=5',
+    country: 'Australia',
+  },
+];
