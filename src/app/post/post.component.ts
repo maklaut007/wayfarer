2efa47c5895e06c8da22dba05044d535f297f89e
Lorena Rojas <lochan04@gmail.com>
2023-05-25 11:51:35 -0500

    added functionality to search bar

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
-import { posts } from '../data/posts';
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent{
   @Input() city: string | null = '';
-  posts: any = posts;
+  posts: any;
+  searchSubject = new Subject();
+
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit() {
+    this.searchService.getFilteredPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
 }
