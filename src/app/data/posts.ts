e97010a4eb26facec6b28c789c8229475bdff879
Lorena Rojas <lochan04@gmail.com>
2023-05-26 12:28:03 -0500

    added css to posts

--- src/app/data/posts.ts
+++ src/app/data/posts.ts
@@ -36,6 +36,36 @@ export const posts: Posts[] = [
         cityId: 4,
         title: 'Adventures in Sydney',
         author: 'Obinna Umerah',
+        content: 'Lorem ipsum dolor sit amet. Id voluptatem culpa et eligendi internos ex deleniti animi. Rem quas dolore eum molestias tenetur in aliquid quod et voluptate culpa qui perferendis nisi ut impedit necessitatibus. Vel magni mollitia eum fugiat sequi qui quaerat perspiciatis qui suscipit dolor non quia doloribus aut consequatur omnis et aliquid soluta! In velit rerum qui voluptates incidunt aut dignissimos optio est explicabo adipisci qui modi neque.'
+    },
+    {   id: 5,
+        cityId: 1,
+        title: 'Adventures in San Francisco part 2',
+        author: 'Lorena Rojas',
+        content: 'Visit San Francisco'
+    },
+
+    {
+        id: 6,
+        cityId: 2,
+        title: 'Adventures in London part 2',
+        author: 'Obinna Umerah',
+        content: 'Visit London'
+    },
+
+    {
+        id: 7,
+        cityId: 3,
+        title: 'Adventures in Seattle part 2',
+        author: 'Maksym Zinchenko',
+        content: 'Visit Seattle'
+    },
+
+    {
+        id: 8,
+        cityId: 4,
+        title: 'Adventures in Sydney part 2',
+        author: 'Obinna Umerah',
         content: 'Visit Sydney'
     }
 ]
